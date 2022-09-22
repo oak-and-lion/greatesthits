ac351a7e0977907467ff92afad0ddf3df5b921b1
Christian Strama <oakandlionsociety@gmail.com>
2022-09-21 21:21:22 -0400

    bug fixes

--- greatesthitsconfig.js
+++ greatesthitsconfig.js
@@ -1,9 +1,6 @@
 // prod Config
-console.log("using Prod Config");
-// check for https protocol
-if (location.protocol !== 'https:') {
-    location.replace(`https:${location.href.substring(location.protocol.length)}`);
-} else {
-    // set the backend API
-    greatest.apiUrl = "https://childsheartyoga.com/BandFunctions.aspx";
-}
+console.log("using Dev Config");
+// set the backend API
+greatest.apiUrl = "http://localhost:51882/BandFunctions.aspx"; //"https://childsheartyoga.com/BandFunctions.aspx";
+
+greatestLoad.loadScript();
