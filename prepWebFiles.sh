5a108f74242a66049fa5e30d2914b69f939cc46d
Christian Strama <oakandlionsociety@gmail.com>
2022-10-05 21:40:55 -0400

    minify html

--- prepWebFiles.sh
+++ prepWebFiles.sh
@@ -9,6 +9,6 @@ uglifyjs greatesthits.js -o min/greatesthits.js
 uglifyjs greatesthitsconfig.js -o min/greatesthitsconfig.js
 uglifyjs greatesthitsfirstcall.js -o min/greatesthitsfirstcall.js
 uglifycss greatesthits.css --output min/greatesthits.css
-cp greatesthits.html min/greatesthits.html
+html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true -o min/greatesthits.html greatesthits.html
 ls -l greatesthits*.*
 ls -l min/
